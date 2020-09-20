bcc70565efc9b3cf266ea50e518c54838408a9a6
Farhan <farhankasmani@live.com>
2020-09-20 18:56:41 +1000

    adding type conversion

--- routes/todos.ts
+++ routes/todos.ts
@@ -5,6 +5,14 @@ export interface Todo {
   text: string;
 }
 
+type RequestBody = {
+  text: string;
+};
+
+type RequestParams = {
+  todoId: string;
+};
+
 let todos: Todo[] = [];
 
 const router = Router();
@@ -14,24 +22,27 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/todo", (req, res, next) => {
+  // type conversion
+  const body = req.body as RequestBody;
   const newTodo: Todo = {
     id: new Date().toISOString(),
-    text: req.body.text,
+    text: body.text,
   };
   todos.push(newTodo);
   res.status(201).json({ message: "Added Todo", todos });
 });
 
 router.put("/todo/:todoId", (req, res, next) => {
-  const tId = req.params.todoId;
-  console.log({ todos });
-  const todoIndex = todos.findIndex(
-    (todo: Todo) => todo.id === tId.toString()
-  );
+  const body = req.body as RequestBody;
+  const params = req.params as RequestParams;
+
+  const tId = params.todoId;
+
+  const todoIndex = todos.findIndex((todo: Todo) => todo.id === tId.toString());
   if (todoIndex > 0) {
     todos[todoIndex] = {
       id: todos[todoIndex].id,
-      text: req.body.text,
+      text: body.text,
     };
     return res.status(100).json({ message: "update is successful", todos });
   }
@@ -39,7 +50,9 @@ router.put("/todo/:todoId", (req, res, next) => {
 });
 
 router.delete("/todo/:todoId", (req, res, next) => {
-  todos = todos.filter((todo) => todo.id !== req.params.todoId);
+  const params = req.params as RequestParams;
+
+  todos = todos.filter((todo) => todo.id !== params.todoId);
   res.status(200).json({ message: "Deleted todo", todos });
 });
 
