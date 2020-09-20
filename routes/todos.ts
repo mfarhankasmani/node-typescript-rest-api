65a8d1acd7761dd2f8cba98b7b0844a845a9b316
Farhan <farhankasmani@live.com>
2020-09-20 18:40:55 +1000

    Test Todo api

---
+++ routes/todos.ts
@@ -0,0 +1,46 @@
+import { Router } from "express";
+
+export interface Todo {
+  id: string;
+  text: string;
+}
+
+let todos: Todo[] = [];
+
+const router = Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({ todos });
+});
+
+router.post("/todo", (req, res, next) => {
+  const newTodo: Todo = {
+    id: new Date().toISOString(),
+    text: req.body.text,
+  };
+  todos.push(newTodo);
+  res.status(201).json({ message: "Added Todo", todos });
+});
+
+router.put("/todo/:todoId", (req, res, next) => {
+  const tId = req.params.todoId;
+  console.log({ todos });
+  const todoIndex = todos.findIndex(
+    (todo: Todo) => todo.id === tId.toString()
+  );
+  if (todoIndex > 0) {
+    todos[todoIndex] = {
+      id: todos[todoIndex].id,
+      text: req.body.text,
+    };
+    return res.status(100).json({ message: "update is successful", todos });
+  }
+  res.status(404).json({ message: "Could not find todo for this id." });
+});
+
+router.delete("/todo/:todoId", (req, res, next) => {
+  todos = todos.filter((todo) => todo.id !== req.params.todoId);
+  res.status(200).json({ message: "Deleted todo", todos });
+});
+
+export default router;
