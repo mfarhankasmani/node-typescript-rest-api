0d8d9054a4ea8b1655cebe987980415a5ea45c26
Farhan <farhankasmani@live.com>
2020-09-21 21:25:39 +1000

    connecting with frontend and validating the post request

--- src/controllers/feed.ts
+++ src/controllers/feed.ts
@@ -1,23 +1,53 @@
 import { RequestHandler } from "express";
-
+import { validationResult } from "express-validator";
 export interface PostBody {
+  _id: string;
   title: string;
   content: string;
+  imageUrl: string;
+  creator: {
+    name: string;
+  };
+  createdAt: Date;
 }
 
 export const getPosts: RequestHandler = (req, res, next) => {
+  const posts: PostBody[] = [
+    {
+      _id: "1",
+      title: "First Post",
+      content: "This is the first post!!",
+      imageUrl: "images/book.jpg",
+      creator: {
+        name: "farhan",
+      },
+      createdAt: new Date(),
+    },
+  ];
+
   res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the first post!" }],
+    posts,
   });
 };
 
 export const createPost: RequestHandler = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation is failed, entered correct data",
+      errors: errors.array(),
+    });
+  }
   const { title, content } = req.body as PostBody;
   // Create post in database
   res.status(201).json({
     message: "Post created!",
-    id: new Date().toISOString(),
-    title,
-    content,
+    post: {
+      _id: new Date().toISOString(),
+      title,
+      content,
+      creator: { name: "farhan" },
+      createdAt: new Date(),
+    },
   });
 };
--- src/routes/feed.ts
+++ src/routes/feed.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
+import { body } from "express-validator";
 
 import { createPost, getPosts } from "../controllers/feed";
 
 const router = Router();
 
 router.get("/posts", getPosts);
-router.post("/post", createPost);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  createPost
+);
 
 export default router;
